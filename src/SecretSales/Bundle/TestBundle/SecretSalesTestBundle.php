223ceb2c4bf2c17baa5061b180b91495301d7228
BRAMILLE Sébastien <sebastien.bramille@gmail.com>
2015-12-27 14:48:16 +0000

    Add default bundle configuration

---
+++ src/SecretSales/Bundle/TestBundle/SecretSalesTestBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SecretSales\Bundle\TestBundle;
+
+use SecretSales\Bundle\TestBundle\DependencyInjection\CompilerPass\HydrateProviderContainerCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class SecretSalesTestBundle
+ */
+class SecretSalesTestBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new HydrateProviderContainerCompilerPass());
+    }
+}
