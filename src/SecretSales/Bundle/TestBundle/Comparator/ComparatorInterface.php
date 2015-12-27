6ac90e8fa399cd93f5c64e4dc59f0bcfc4112272
BRAMILLE Sébastien <sebastien.bramille@gmail.com>
2015-12-27 14:50:02 +0000

    Add comparator

---
+++ src/SecretSales/Bundle/TestBundle/Comparator/ComparatorInterface.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the test project
+ *
+ * (c) BRAMILLE Sébastien <sebastien.bramille@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SecretSales\Bundle\TestBundle\Comparator;
+
+/**
+ * Interface ComparatorInterface
+ */
+interface ComparatorInterface
+{
+    /**
+     * Compare 2 elements
+     *
+     * @param mixed $a
+     * @param mixed $b
+     *
+     * @return int -1 if $a should precede $b
+     *              1 if $b should precede $a
+     *              0 if considered equal
+     */
+    public function compare($a, $b);
+}
---
+++ src/SecretSales/Bundle/TestBundle/Comparator/ReverseNumericComparator.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the test project
+ *
+ * (c) BRAMILLE Sébastien <sebastien.bramille@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SecretSales\Bundle\TestBundle\Comparator;
+
+/**
+ * Reverse (DESC instead of ASC) the sorting of 2 elements
+ *
+ * Class ReverseNumericComparator
+ */
+class ReverseNumericComparator implements ComparatorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function compare($a, $b)
+    {
+        if ($a === $b) {
+            return 0;
+        }
+
+        return $a > $b ? -1 : 1;
+    }
+}
