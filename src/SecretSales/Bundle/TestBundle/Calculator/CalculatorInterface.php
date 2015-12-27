78d149f440f7c778f595ee14d89e9376f91e9df1
BRAMILLE Sébastien <sebastien.bramille@gmail.com>
2015-12-27 14:50:23 +0000

    Add calculator

---
+++ src/SecretSales/Bundle/TestBundle/Calculator/CalculatorInterface.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of the test project
+ *
+ * (c) BRAMILLE Sébastien <sebastien.bramille@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SecretSales\Bundle\TestBundle\Calculator;
+
+/**
+ * Interface CalculatorInterface
+ */
+interface CalculatorInterface
+{
+    /**
+     * Initialize the calculator with an array to process
+     *
+     * @param array $posts
+     *
+     * @return $this
+     */
+    public function initialize(array $posts);
+
+    /**
+     * Calculate and return the result
+     *
+     * @return mixed
+     */
+    public function calculate();
+}
---
+++ src/SecretSales/Bundle/TestBundle/Calculator/FrequencyCalculator.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * This file is part of the test project
+ *
+ * (c) BRAMILLE Sébastien <sebastien.bramille@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SecretSales\Bundle\TestBundle\Calculator;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+/**
+ * Calculate the words frequency
+ *
+ * Class FrequencyCalculator
+ */
+class FrequencyCalculator implements CalculatorInterface
+{
+    /**
+     * @var string
+     */
+    protected $string;
+
+    /**
+     * @var DataTransformerInterface
+     */
+    private $dataTransformer;
+
+    /**
+     * FrequencyCalculator constructor.
+     *
+     * @param DataTransformerInterface $dataTransformer
+     */
+    public function __construct(DataTransformerInterface $dataTransformer)
+    {
+        $this->dataTransformer = $dataTransformer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize(array $posts)
+    {
+        $this->setString($this->dataTransformer->reverseTransform($posts));
+
+        return $this;
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return $this
+     */
+    public function setString($string)
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getString()
+    {
+        return $this->string;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function calculate()
+    {
+        return array_count_values(str_word_count($this->getString(), 1));
+    }
+}
