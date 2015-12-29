2b2100d3c89e9e7d8f3cf90ce36182cbb1a8d23b
BRAMILLE Sébastien <sebastien.bramille@gmail.com>
2015-12-29 09:50:09 +0000

    Add SorterInterface

---
+++ src/SecretSales/Bundle/TestBundle/Sorter/SorterInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the test project
+ *
+ * (c) BRAMILLE Sébastien <sebastien.bramille@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SecretSales\Bundle\TestBundle\Sorter;
+
+/**
+ * Interface SorterInterface
+ */
+interface SorterInterface
+{
+    /**
+     * Execute a sorting function on the $items array
+     *
+     * @param array $items
+     *
+     * @return array
+     */
+    public function sort(array $items);
+}
